89c7f45462bfaa0cd0d81116b9a3c1b3a34aea1e
patricianobrecc <71797649+patricianobrecc@users.noreply.github.com>
2020-09-24 09:14:06 -0300

    Update ativexercicio5.php

--- ativexercicio5.php
+++ ativexercicio5.php
@@ -9,24 +9,26 @@
             <?php
             
             $nota1 = 5;
-            $nota2 = 6;
-            $nota3 = 9;
-            $notafinal = ($nota1 + $nota2 + $nota3);
+            $nota2 = 5;
+            $nota3 = 5;
+            $notafinal = (($nota1 + $nota2 + $nota3) / 3 );
             echo ("Sua nota da unidade 1 foi $nota1 <br/>");
             echo ("Sua nota da unidade 2 foi $nota2 <br/>");
+            echo ("Sua nota da unidade 3 foi $nota3 <br/>");
             
             if ($notafinal >= 6)
             {
                 echo ("Voce esta aprovado!");
             }
-            elseif ($notafinal < 6)
-            {
-                echo ("Voce esta de recuperacao!");
-            }
-            else ($notafinal <= 3)
+            elseif ($notafinal <= 3)
             {
                 echo ("Voce esta reprovado!");
             }
+            elseif ($notafinal < 6 )
+            {
+                echo ("Voce esta de recuperacao!");
+            }
+            
 
             ?>
         </p>
